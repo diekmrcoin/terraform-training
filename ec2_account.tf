55e0dbc1fafcee31a2afb6e94989298dda8ebd30
Diego Maroto <diego.diemg@gmail.com>
2022-04-18 17:27:22 +0200

    Added terraform providers and its versions.

--- ec2_account.tf
+++ ec2_account.tf
@@ -1,6 +1,17 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.9"
+    }
+  }
+
+  required_version = ">= 1.1.8"
+}
+
 provider "aws" {
   region = "eu-west-3"
-  alias  = "terraform-course"
+  profile  = "terraform-course"
 }
 
 resource "aws_vpc" "terraform_course" {
